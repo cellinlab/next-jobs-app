db947f89329dcd9155037135f45692928fbc95a7
cell <cellinlab@gmail.com>
2023-04-21 15:29:20 +0800

    feat: jobs page in dashboard

---
+++ src/pages/dashboard/jobs/index.tsx
@@ -0,0 +1,44 @@
+import { ReactElement } from "react";
+import { Heading, HStack } from "@chakra-ui/react";
+import { PlusSquareIcon } from "@chakra-ui/icons";
+
+import { Link } from "@/components/link";
+import { Loading } from "@/components/loading";
+import { Seo } from "@/components/seo";
+import { JobsList } from "@/features/jobs";
+import { DashboardLayout } from "@/layouts/dashboard-layout";
+
+import { useJobs, useUser } from "@/testing/test-data";
+
+const DashboardJobsPage = () => {
+  const user = useUser();
+  const jobs = useJobs(user.data?.organizationId ?? "");
+
+  if (jobs.isLoading) {
+    return <Loading />;
+  }
+
+  if (!user.data) return null;
+
+  return (
+    <>
+      <Seo title="Jobs" />
+      <HStack mb="8" justify="space-between" align="center">
+        <Heading>Jobs</Heading>
+        <Link href={`/dashboard/jobs/create`} icon={<PlusSquareIcon />} variant="solid">
+          Create Job
+        </Link>
+      </HStack>
+      <JobsList
+        jobs={jobs.data || []}
+        isLoading={jobs.isLoading}
+        organizationId={user.data.organizationId}
+        type="dashboard"
+      />
+    </>
+  );
+};
+
+DashboardJobsPage.getLayout = (page: ReactElement) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default DashboardJobsPage;
