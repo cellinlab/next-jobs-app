7406937ebcbff738c0c89c2f08660eeec762e711
cell <cellinlab@gmail.com>
2023-04-21 16:28:24 +0800

    feat: update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,35 @@
-import { Button } from "@/components/button";
-import { InputField } from "@/components/form";
+import { Center, Heading, Text, VStack } from "@chakra-ui/react";
+
 import { Link } from "@/components/link";
+import { Seo } from "@/components/seo";
 
 const LandingPage = () => {
   return (
     <>
-      <Button>Click me</Button>
-      <br />
-      <InputField label="name" />
-      <br />
-      <Link href="/">Home</Link>
+      <Seo title="Jobs App" />
+      <Center h="full" flexDirection="column">
+        <VStack maxW="3xl" spacing="8">
+          <Heading size="3xl">
+            Welcome to the{" "}
+            <Text as="span" color="blue.500">
+              Jobs App
+            </Text>
+          </Heading>
+          <Text
+            fontSize={{
+              base: "lg",
+              md: "xl",
+            }}
+            maxW="2xl"
+            color="muted"
+          >
+            Manage your careers pages
+          </Text>
+          <Link href="/dashboard/jobs" variant="solid">
+            Get started
+          </Link>
+        </VStack>
+      </Center>
     </>
   );
 };
