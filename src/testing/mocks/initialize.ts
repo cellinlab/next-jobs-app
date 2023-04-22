3ea5ad28fa1a0c6310d539e5153c3c5f941d4de1
cell <cellinlab@gmail.com>
2023-04-22 23:45:00 +0800

    feat: configuring data models

--- src/testing/mocks/initialize.ts
+++ src/testing/mocks/initialize.ts
@@ -1,13 +1,14 @@
 import { IS_SERVER } from '@/config/constants';
 
+import { seedDb } from './seed-db';
+
 export const initializeMocks = async () => {
   if (IS_SERVER) {
-    console.log('Initializing server mocks');
     const { server } = await import('./server');
     server.listen();
   } else {
-    console.log('Initializing browser mocks');
     const { worker } = await import('./browser');
     worker.start();
   }
+  seedDb();
 };
